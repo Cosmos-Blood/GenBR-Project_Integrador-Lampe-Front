cf30d82deb1b7c3bbcc028340e0e724ed1c361f4
Eduardo <edu3839@hotmail.com>
2021-11-03 15:20:31 -0300

    [Feature Eduardo - Task: Update entrar | 03.11.21]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrarComponent } from './entrar/entrar.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
-  {path:"sobre",component: SobreComponent}
+  {path:"sobre",component: SobreComponent},
+  {path:"entrar",component: EntrarComponent}
 ];
 
 @NgModule({
