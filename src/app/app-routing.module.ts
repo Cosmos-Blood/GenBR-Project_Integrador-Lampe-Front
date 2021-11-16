c5b04e16c10ba3bf3daf2c2f6a6b7418450b07b5
WillianX47 <willianx47@gmail.com>
2021-11-16 15:54:49 -0300

    [Feature Will - Task: usuario-edit Component | 16.11.21]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { UsuarioEditComponent } from './edit/usuario-edit/usuario-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { FeedComponent } from './feed/feed.component';
 import { SobreComponent } from './sobre/sobre.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path:"entrar",component: EntrarComponent},
   {path: "cadastrar", component: CadastrarComponent},
   {path: "feed", component: FeedComponent},
+  {path: "usuario-edit", component: UsuarioEditComponent},
   {path: "postagem-edit/:id", component: PostagemEditComponent},
   {path: "postagem-delete/:id", component: PostagemDeleteComponent}
 ];
