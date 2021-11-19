a3ebfb617a4ad194269d5fa9d80fbec86c3fa056
Zoraten <cosmos_blood@outlook.com>
2021-11-19 13:38:16 -0300

    [Feature Guilherme - Task: Update perfil component | 19.11.21]

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { PostagemModel } from '../model/PostagemModel';
+import { TemaModel } from '../model/TemaModel';
+import { UsuarioModel } from '../model/UsuarioModel';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-perfil',
@@ -9,8 +15,16 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class PerfilComponent implements OnInit {
 
+  listaDeTemas: TemaModel[]
+  tema: TemaModel = new TemaModel()
+  listaDePostagens: PostagemModel[]
+  postagem: PostagemModel = new PostagemModel()
+  usuario: UsuarioModel = new UsuarioModel()
+  idUsuario = environment.id
+  idTema: number
+
   constructor(
-    private router: Router
+    private router: Router, private auth: AuthService, private postagemService: PostagemService, private temaService: TemaService
   ) { }
 
 
@@ -20,6 +34,30 @@ export class PerfilComponent implements OnInit {
       this.router.navigate(['/entrar']);
     }
 
+    this.getAllPostagens()
+    this.getAllTemas()
   }
 
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: PostagemModel[]) => {
+      this.listaDePostagens = resp
+    })
+  }
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: TemaModel) =>{
+      this.tema = resp
+    })
+  }
+
+   getAllTemas(){
+     this.temaService.getAllTemas().subscribe((resp: TemaModel[])=>{
+      this.listaDeTemas = resp
+     })
+   }
+  
+  findByIdUser(){
+    this.auth.getUsuarioById(this.idUsuario).subscribe((resp: UsuarioModel) => {
+      this.usuario = resp
+    })
+  }
 }
