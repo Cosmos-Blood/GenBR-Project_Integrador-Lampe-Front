6282cdbfc3ee56fc41ba261107c2c111c37cc926
MacaulyFragoso <macaulyaparecido@gmail.com>
2021-11-19 12:20:57 -0300

    [Feature Macauly - RouterLink Eventos & Perfil | 19.11.21]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { UsuarioEditComponent } from './edit/usuario-edit/usuario-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { EventosComponent } from './eventos/eventos.component';
 import { FeedComponent } from './feed/feed.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 const routes: Routes = [
   {path:"entrar",component: EntrarComponent},
@@ -13,7 +15,9 @@ const routes: Routes = [
   {path: "feed", component: FeedComponent},
   {path: "usuario-edit", component: UsuarioEditComponent},
   {path: "postagem-edit/:id", component: PostagemEditComponent},
-  {path: "postagem-delete/:id", component: PostagemDeleteComponent}
+  {path: "postagem-delete/:id", component: PostagemDeleteComponent},
+  {path: "eventos", component: EventosComponent},
+  {path:"perfil", component: PerfilComponent}
 ];
 
 @NgModule({
